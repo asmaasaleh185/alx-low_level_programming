445b0a21bc9d6b9c4cbe314bc56fea0dbc85680f
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-03-20 15:27:33 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,10 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	fprintf(stderr, "size of a char: %u byte(s)\n", sizeof(a));
-	fprintf(stderr, "size of an int: %u byte(s)\n", sizeof(b));
-	fprintf(stderr, "size of a long int: %u byte(s)\n", sizeof(c));
-	fprintf(stderr, "size of a long long int: %u byte(s)\n", sizeof(d));
-	fprintf(stderr, "size of a float: %u byte(s)\n", sizeof(e));
+	printf("size of a char: %u byte(s)\n", sizeof(char));
+	printf("size of an int: %u byte(s)\n", sizeof(int));
+	printf("size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
