0696e2baf3b4559d53c021af16d3749835d0920e
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-03-22 07:01:39 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,16 @@
  * main-Entry point of my code
  * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
-        _putchar('_');
-        _putchar('P');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
