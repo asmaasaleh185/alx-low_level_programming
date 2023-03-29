729e2f5a1670338c71ab99eac43f23461344bfd7
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-03-29 12:29:56 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string followed by a new line
+ * @str: the string to be printed
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	char *y = str;
+	int i = 0;
+
+	while (*y != '\0')
+	{
+		len++;
+		y++;
+	}
+	if (len % 2 == 0)
+	{
+	for (i = (len / 2); ((i >= len / 2) && (i <= len)); i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+	}
+	else if (len == 0)
+	{
+	}
+	else
+	{
+	for (i = ((len - 1) / 2); ((i >= (len - 1) / 2) && (i <= len)); i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+	}
+}
+
