a705a142a125f4c167d4c2f9674883a03264a7b9
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-03-29 12:43:46 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,22 +17,18 @@ void puts_half(char *str)
 	}
 	if (len % 2 == 0)
 	{
-	for (i = (len / 2); ((i >= len / 2) && (i <= len)); i++)
+	for (i = (len / 2); ((i >= (len / 2)) && (i <= len)); i++)
 	{
 	_putchar(str[i]);
 	}
 	_putchar('\n');
 	}
-	else if (len == 0)
-	{
-	}
 	else
 	{
-	for (i = ((len - 1) / 2); ((i >= (len - 1) / 2) && (i <= len)); i++)
+	for (i = ((len - 1) / 2); ((i >= ((len - 1) / 2)) && (i <= len)); i++)
 	{
 	_putchar(str[i]);
 	}
 	_putchar('\n');
 	}
 }
-
