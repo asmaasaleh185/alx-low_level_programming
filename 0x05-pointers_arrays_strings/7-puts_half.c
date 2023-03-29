ac0d702cb8fd8bc047a723154a4beeb082d89021
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-03-29 12:53:46 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,6 +3,7 @@
  * puts_half - prints half of a string followed by a new line
  * @str: the string to be printed
  * Return: void
+ * Description: if odd number of char, print (length - 1) / 2
  */
 void puts_half(char *str)
 {
@@ -15,20 +16,9 @@ void puts_half(char *str)
 		len++;
 		y++;
 	}
-	if (len % 2 == 0)
-	{
 	for (i = (len / 2); ((i >= (len / 2)) && (i <= len)); i++)
 	{
 	_putchar(str[i]);
 	}
 	_putchar('\n');
-	}
-	else
-	{
-	for (i = ((len - 1) / 2); ((i >= ((len - 1) / 2)) && (i <= len)); i++)
-	{
-	_putchar(str[i]);
-	}
-	_putchar('\n');
-	}
 }
