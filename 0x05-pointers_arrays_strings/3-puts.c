1dedfbe1dbbf82e92e75108da1f035374883082d
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-03-29 05:17:29 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts - a function that prints a string
+ * _puts - prints a string followed by a new line, to stdout
  * @str: the string to be printed
  * Return: void
  */
