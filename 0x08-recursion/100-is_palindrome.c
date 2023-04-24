06457186724e364935007d669c9c5a00907bbccb
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-04-23 17:42:25 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -22,7 +22,8 @@ int last_index(char *s)
 int is_palindrome(char *s)
 {
 	int end = last_index(s);
-	return(check(s, 0, end - 1, end % 2));
+
+	return (check(s, 0, end - 1, end % 2));
 }
 /**
  * check -checker for the palindrome
@@ -35,9 +36,9 @@ int is_palindrome(char *s)
 int check(char *s, int start, int end, int mod)
 {
 	if ((start == end && mod != 0) || (start == end + 1 && mod == 0))
-		return(1);
+		return (1);
 	else if (s[start] != s[end])
 		return (0);
 	else
-		return (check (s, start + 1, end - 1, mod));
+		return (check(s, start + 1, end - 1, mod));
 }
