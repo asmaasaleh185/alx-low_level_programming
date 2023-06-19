c3e76228c893012a0f1e4cdedbba1a27ba433f56
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-06-19 15:04:57 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib>
+/**
+ * free_dog -frees dogs
+ * @d: the dog to be free
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
