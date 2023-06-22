c930fc9eaa01a5e294cba05e87d39d8338db046c
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-06-21 17:13:13 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @size: size of an array
+ * @cmp: pointer of the function
+ * @array: int array
+ * Return: int index
+ */
+ int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		for (; i < size ; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	return (-1);
+}
