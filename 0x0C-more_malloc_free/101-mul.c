9d87c61ac83c2e1ce5b2c80c6d697389b64973c1
asmaasaleh185 <asmaasaleh185@gmail.com>
2023-07-13 14:40:51 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,79 +1,101 @@
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line
- * @s: string pointer to be printed
- * Return: void
+ * _isdigit - checks if character is digit
+ * @c: character to be checked
+ * Return: 1 (digit, 0 (otherwise)
  */
-void _puts(char *s)
+int _isdigit(int c)
 {
-	int i = 0;
-
-	while (s[i])
-	{
-		_putchar(s[i]);
-		i++;
-	}
+	return (c >= '0' && c <= '9');
 }
-
 /**
- * _stoi -convert a string to an int
+ * _strlen - find the length
  * @s: string
- * Return: converted int
+ * Return: length
  */
-int _stoi(const char *s)
+int _strlen(char *s)
 {
-	int sign = 1;
-	unsigned long int res = 0, firstNum, i;
+	int len = 0;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
-	}
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		res *= 10;
-		res += (s[i] - 48);
-	}
-	return (sign * res);
-}
-
-/**
- * _print - prints int
- * @n: int
- * Return: 0
- */
-void _print(unsigned long int n)
-{
-	unsigned long int div = 1, i, res;
-
-	for (i = 0; n / div > 9; i++, div *= 10)
+	for (; s[len] != '\0'; len++)
 		;
-	for (; div >= 1; n %= div, div /= 10)
-	{
-		res = n / div;
-		_putchar('0' + res);
-	}
+	return (len);
 }
-
 /**
- * main -print the result of multiplication, followed by a new line
- * @argc: int
+ * _mul -multiply two string numbers
+ * @s1: first string number
+ * @s2: second string number
+ * Return: multiplication
+ */
+char *_mul(char *s1, char *s2)
+{
+	char *r;
+	int l1, l2, a, b, c, x;
+
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	r = malloc(a = x = l1 + l2);
+
+	if (!r)
+		printf("Error\n"), exit(98);
+	while (a--)
+		r[a] = 0;
+	for (l1--; l1 >= 0; l1--)
+	{
+		if (!_isdigit(s1[l1]))
+		{
+			free(r);
+			printf("Error\n"), exit(98);
+		}
+		a = s1[l1] - '0';
+		c = 0;
+
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			if (!_isdigit(s2[l2]))
+			{
+				free(r);
+				printf("Error\n"), exit(98);
+			}
+			b = s2[l2] - '0';
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
+			c /= 10;
+		}
+		if (c)
+			r[l1 + l2 + 1] += c;
+	}
+	return (r);
+}
+/**
+ * main - multiply two strin numbers
+ * @argc: number of srguments
  * @argv: list
  * Return: 0
  */
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
-	(void)argc;
+	char *r;
+	int a, c, x;
 
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	r = _mul(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+
+	while (c < x)
 	{
-		_puts("Error ");
-		exit(98);
+		if (r[c])
+			a = 1;
+		if (a)
+			_putchar(r[c] + '0');
+		c++;
 	}
-	_print(_stoi(argv[1]) * _stoi(argv[2]));
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
+	free(r);
 	return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,8 +10,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void _puts(char *s);
-int _stoi(const char *s);
-void _print(unsigned long int n);
+int _isdigit(int c);
+int _strlen(char *s);
+char *_mul(char *s1, char *s2);
 
 #endif
